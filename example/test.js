265b2670a9a71ec6aced283dc7f2242d6738b458
Chee Yong <81070048+leecheeyong@users.noreply.github.com>
2025-07-14 19:52:49 +0000

    added examples

--- example/test.js
+++ example/test.js
@@ -1,10 +1,9 @@
 import { exec } from 'child_process';
 
-// This script executes a command to run a Node.js application
+// This script executes ../index.js to run a test case
 // that processes images in a specified directory and outputs results to a CSV file.
 // Images in the './images' directory must be compressed to a smaller size before running the script.
 // Use JpegOptimizer e.g jpegoptim --size=100k *.JPG
-// description.csv will have the output of the processed images.
 
 exec('node ../index.js --dir ./images --output description.csv -w 5000', (error, stdout, stderr) => {
   if (error) {
@@ -18,3 +17,4 @@ exec('node ../index.js --dir ./images --output description.csv -w 5000', (error,
   console.log(`${stdout.trim()}`);
 });
 
+// description.csv will have the output of the processed images.
